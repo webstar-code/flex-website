403f2254ac39d2e90ca7510b0e437d2d7807d0ab
webstar-code <bhaveshrc2001@gmail.com>
2021-11-08 13:34:00 +0530

    minor changes

---
+++ src/assets/index.js
@@ -0,0 +1,40 @@
+import FLEX_LOGO from './logos/flex-logo.png';
+import FLEX_LOGO2X from './logos/flex-logo2x.png';
+
+import BANNER from './images/banner.png';
+import BANNER2X from './images/banner2x.png';
+import HUNGER_MOCKUP from './images/hunger2x.png'
+import ANA_MOCKUP from './images/ana2x.png'
+import HEALTHY_MOCKUP from './images/healthy2x.png'
+
+import ALBANIA from './images/albania.svg';
+import AUSTRALIA from './images/australia.svg';
+import CANADA from './images/canada.svg';
+import INDIA from './images/india.svg';
+import UAE from './images/uae.svg';
+import NIGERIA from './images/nigeria.svg';
+import NETHERLANDS from './images/netherlands.svg';
+import USA from './images/usa.svg';
+import MACEDONIA from './images/macedonia.svg'
+
+
+
+export {
+    FLEX_LOGO,
+    FLEX_LOGO2X,
+    BANNER,
+    BANNER2X,
+    HUNGER_MOCKUP,
+    ANA_MOCKUP,
+    HEALTHY_MOCKUP,
+
+    ALBANIA,
+    AUSTRALIA,
+    CANADA,
+    INDIA,
+    UAE,
+    NIGERIA,
+    NETHERLANDS,
+    USA,
+    MACEDONIA
+}
