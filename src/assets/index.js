e7ce40f3ef5cca9c56093e9286faa66c4e7a9588
webstar-code <bhaveshrc2001@gmail.com>
2021-11-08 14:50:53 +0530

    main page portofolio changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -17,8 +17,31 @@ import NETHERLANDS from './images/netherlands.svg';
 import USA from './images/usa.svg';
 import MACEDONIA from './images/macedonia.svg'
 
+import ana1 from './products/ana1.png';
+import ana2 from './products/ana2.png';
+import ana3 from './products/ana3.png';
 
+import domineum1 from './products/domineum1.png';
+import domineum2 from './products/domineum2.png';
+import domineum3 from './products/domineum3.png';
 
+import hunger1 from './products/hunger1.png';
+import hunger2 from './products/hunger2.png';
+import hunger3 from './products/hunger3.png';
+
+import litenlamps1 from './products/litenlamps1.png';
+import litenlamps2 from './products/litenlamps2.png';
+import litenlamps3 from './products/litenlamps3.png';
+
+import onehealthwell1 from './products/onehealthwell1.png';
+import onehealthwell2 from './products/onehealthwell2.png';
+import onehealthwell3 from './products/onehealthwell3.png';
+
+import prajwala1 from './products/prajwala1.png';
+import prajwala2 from './products/prajwala2.png';
+import prajwala3 from './products/prajwala3.png';
+
+import UNION from './icons/Union.svg';
 export {
     FLEX_LOGO,
     FLEX_LOGO2X,
@@ -36,5 +59,32 @@ export {
     NIGERIA,
     NETHERLANDS,
     USA,
-    MACEDONIA
+    MACEDONIA,
+
+    ana1,
+    ana2,
+    ana3,
+
+    domineum1,
+    domineum2,
+    domineum3,
+
+    hunger1,
+    hunger2,
+    hunger3,
+
+    litenlamps1,
+    litenlamps2,
+    litenlamps3,
+
+    onehealthwell1,
+    onehealthwell2,
+    onehealthwell3,
+
+    prajwala1,
+    prajwala2,
+    prajwala3,
+
+    UNION
+
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,84 +1,81 @@
 import React from 'react'
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import liteNLamp from '../assets/lite-lamp.png'
-import domineum  from '../assets/dominum.png'
-import gokul  from '../assets/gokul.png'
-import healthy  from '../assets/healthy.png'
-import jd  from '../assets/jd.png'
-import kutra  from '../assets/kutra.png'
-import {AiOutlineEye} from 'react-icons/ai'
-const Portfolio = ({setIndex,index}) => {
+import domineum from '../assets/dominum.png'
+import gokul from '../assets/gokul.png'
+import healthy from '../assets/healthy.png'
+import jd from '../assets/jd.png'
+import kutra from '../assets/kutra.png'
+import { AiOutlineEye } from 'react-icons/ai'
+import { FaLongArrowAltRight } from 'react-icons/fa'
+
+import {
+    ana1, ana2, ana3,
+    domineum1, domineum2, domineum3,
+    hunger1, hunger2, hunger3,
+    litenlamps1, litenlamps2, litenlamps3,
+    onehealthwell1, onehealthwell2, onehealthwell3,
+    prajwala1, prajwala2, prajwala3, UNION
+} from '../assets'
+
+const Portfolio = ({ setIndex, index }) => {
     const history = useHistory()
-    const pushToProtFolio=(num)=>{
-      setIndex(num)
-      history.push('/portfolio')
+    const pushToProtFolio = (num) => {
+        setIndex(num)
+        history.push('/portfolio')
     }
     return (
         <div className="portfolio">
             <h2>PORTFOLIO</h2>
-            <div className="profolio-items">
-                <div className="protfilo-cards">
-                    <div className="portfolio-card">
-                        <img src={liteNLamp} alt="" />
-                        <div className="por-text">
-                            <h3>Lite N Lamps</h3>
-                            <div className="eye" onClick={()=>pushToProtFolio(0)}>
-                                <AiOutlineEye/>
-                            </div>
-                        </div>
+            <div className="portfolio-items">
+                <div className="portfolio-item">
+                    <div className="images">
+                        <img src={hunger1} className="image-1" />
+                        <img src={hunger2} className="image-2" />
+                        <img src={hunger3} className="image-3" />
                     </div>
-                    <div className="portfolio-card">
-                         <img src={domineum} alt="" />
-                         <div className="por-text">
-                            <h3>Domineum</h3>
-                            <div className="eye" onClick={()=>pushToProtFolio(1)}>
-                                <AiOutlineEye/>
-                            </div>
-                        </div>
+                    <div className="des">
+                        <h1>Restro Solutions</h1>
+                        <img src={UNION} />
                     </div>
                 </div>
-                <div className="protfilo-cards-2">
-                <div className="portfolio-card">
-                        <img src={gokul} alt="" />
-                        <div className="por-text">
-                            <h3>Gokul Diagnostics</h3>
-                            <div className="eye">
-                                <AiOutlineEye/>
-                            </div>
-                        </div>
+                <div className="portfolio-item reverse">
+                    <div className="images">
+                        <img src={onehealthwell1} className="image-1" />
+                        <img src={onehealthwell2} className="image-2" />
+                        <img src={onehealthwell3} className="image-3" />
                     </div>
-                <div className="portfolio-card margin-right">
-                        <img src={healthy} alt="" />
-                        <div className="por-text">
-                            <h3>Healthy Nutrients</h3>
-                            <div className="eye">
-                                <AiOutlineEye/>
-                            </div>
-                        </div>
+                    <div className="des">
+                        <h1>One health wellness</h1>
+                        <img src={UNION} />
                     </div>
                 </div>
-
-                <div className="protfilo-cards reverse">
-                    <div className="portfolio-card">
-                        <img src={jd} alt="" />
-                        <div className="por-text">
-                            <h3>JD Herbal</h3>
-                            <div className="eye">
-                                <AiOutlineEye/>
-                            </div>
-                        </div>
+                <div className="portfolio-item">
+                    <div className="images">
+                        <img src={ana1} className="image-1" />
+                        <img src={ana2} className="image-2" />
+                        <img src={ana3} className="image-3" />
                     </div>
-                    <div className="portfolio-card kurta">
-                         <img src={kutra} alt="" />
-                         <div className="por-text">
-                            <h3>Kurta Ghar</h3>
-                            <div className="eye">
-                                <AiOutlineEye/>
-                            </div>
-                        </div>
+                    <div className="des">
+                        <h1>ANA</h1>
+                        <img src={UNION} />
+                    </div>
+                </div>
+                <div className="portfolio-item reverse">
+                    <div className="images">
+                        <img src={litenlamps1} className="image-1" />
+                        <img src={litenlamps2} className="image-2" />
+                        <img src={litenlamps3} className="image-3" />
+                    </div>
+                    <div className="des">
+                        <h1>Lite n Lamps</h1>
+                        <img src={UNION} />
                     </div>
                 </div>
             </div>
+
+
+
         </div>
     )
 }
