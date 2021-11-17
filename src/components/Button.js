5880385dec377412ba76e9920522125aa7fdf7f4
webstar-code <bhaveshrc2001@gmail.com>
2021-11-17 16:06:23 +0530

    pwa

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+
+const Button = ({ text, path }) => {
+	return (
+		<Link to={path}>
+			<button className="bg-white uppercase border-none outline-none px-10 py-5 md:px-12 md:py-6 rounded-full font-extrabold tracking-wide cursor-pointer shadow-lg text-primary hover:scale-105 transform transition-all">
+				{text}
+			</button>
+		</Link>
+	)
+}
+
+export default Button;
